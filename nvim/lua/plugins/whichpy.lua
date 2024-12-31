c47cd3f0a33d43ada186069f229bbd1574d5aef1
Timóteo Ramos <timoteoramos@ymail.com>
2024-12-31 14:49:40 -0300

    feat: python environment

---
+++ nvim/lua/plugins/whichpy.lua
@@ -0,0 +1,12 @@
+return {
+  "neolooong/whichpy.nvim",
+  dependencies = {
+    "mfussenegger/nvim-dap-python",
+    "ibhagwan/fzf-lua",
+  },
+  opts = {
+    picker = {
+      name = "fzf-lua",
+    },
+  },
+}
