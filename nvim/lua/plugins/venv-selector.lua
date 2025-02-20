a203f5c205e6a59de733dfb2b5d2c62bc42b6b44
Timóteo Ramos <timoteoramos@ymail.com>
2025-02-20 18:05:58 -0300

    feat: venv selector

---
+++ nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,9 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  opts = {},
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+    { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+  },
+}
