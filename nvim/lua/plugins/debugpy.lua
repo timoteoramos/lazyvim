1e3fbd2bd4a261c4f1741ee8dfd3fe27697708fd
Timóteo Ramos <timoteoramos@ymail.com>
2025-10-29 18:10:08 -0300

    feat: debugpy from pipx

--- nvim/lua/plugins/debugpy.lua
+++ nvim/lua/plugins/debugpy.lua
@@ -1,6 +1,6 @@
 return {
   "mfussenegger/nvim-dap-python",
   config = function()
-    require("dap-python").setup("/usr/bin/python3")
+    require("dap-python").setup("~/.local/share/pipx/venvs/debugpy/bin/python")
   end,
 }
